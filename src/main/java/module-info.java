dcf21697ddab092d337fa727770b0c001c54aee5
YouserDan <velodrom19@gmail.com>
2024-12-17 15:54:41 +0300

    сделал подключение к бд

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.course_directory {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens org.example.course_directory to javafx.fxml;
--- src/main/java/org/example/course_directory/StartProgram.java
+++ src/main/java/org/example/course_directory/StartProgram.java
@@ -6,10 +6,19 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import org.example.course_directory.connection.DatabaseConnection;  // Убедитесь, что импортируете правильный класс
 
 public class StartProgram extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
+        DatabaseConnection dbConnection = new DatabaseConnection();
+
+        Connection connection = dbConnection.connectToDatabase();
+
+
+        // Загружаем и отображаем FXML
         FXMLLoader fxmlLoader = new FXMLLoader(StartProgram.class.getResource("auth.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Авторизация");
